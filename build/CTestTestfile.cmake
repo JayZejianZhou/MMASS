085c99fc8946b98bd13b10572ed7211efca91342
zejian <zhouzejian1994@gmail.com>
2020-12-14 13:43:42 -0800

    first add

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/zejian/MASSims/src
+# Build directory: /home/zejian/MASSims/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
