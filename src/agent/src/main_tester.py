a89e282ab33c1c67a259212e229f457cfaf83cbe
zhouzejian1994@gmail.com <rnqPAuYKC?2y6wGeARU=83DQ>
2020-12-14 17:01:27 -0800

    add CARE solution

--- src/agent/src/main_tester.py
+++ src/agent/src/main_tester.py
@@ -1,16 +1,19 @@
 # 2020-12-14 main tester of the Agent.py package. Created and maintained by Zejian Zhou
 from Agent import Agent
 import numpy as np
+from scipy import linalg
 
 def linear_dynamics (state, action):
     a,b=np.array([[-3,2],[1,1]]), [[0],[1]]
     return (a*state + b*action)
 
 def linear_actions (state):
-    R = 1/3
-    P = np.array([[0.5895,1.8216], [1.8216, 8.8188]])
-    a, b = np.array([[-3, 2], [1, 1]]), [[0], [1]]
-    k= R * np.transpose(b)* P
+    R = 3
+    q = np.array([[1,  0], [0, 1]])
+    a = np.array([[-3, 2], [1, 1]])
+    b= np.array([[0], [1]])
+    P = linalg.solve_continuous_are (a, b, q, R)
+    k= 1/R * np.transpose(b)* P
     return -k*state
 
 
