8a4fd6831267c15105a61cf966a8a821a974d938
zhouzejian1994@gmail.com <rnqPAuYKC?2y6wGeARU=83DQ>
2020-12-16 12:53:22 -0800

    working linear dynamics without FPK, with plot

--- src/agent/src/main_tester.py
+++ src/agent/src/main_tester.py
@@ -2,14 +2,43 @@
 from Agent import Agent
 import numpy as np
 from scipy import linalg
-import matplotlib.pyplot as plt
+import utilities
+
+#TODO: python version ADP
+#TODO: connect with the FPK solver
 
 
+dt = 0.1 #delta t to discretize the dynamics in temporal space
+fpk_dt = 1e-8 #delta t for FPK solver
+""" single linear dynamics
+    state -- single vector 
+    action -- single vector
+"""
 def linear_dynamics(state, action):
     a, b = np.array([[-3, 2], [1, 1]]), [[0], [1]]
     return (np.dot(a, state) + b * action)
 
+"""linear dynamics compatible with FPK solver
+    x -- MxN matrix, see notibility note for details
+    y -- MxN matrix, see notibility note for details
+"""
+def linear_dynamics_fpk(x, y):
+    grid_len = x[:, 0].size
+    com = np.array([x, y]) #make the current state
+    #the mesh value of the augmented state vector, see notabilitty
+    Fx = np.zeros((80, 80))
+    Fy = np.zeros((80, 80))
+    for i in range(grid_len):
+        for j in range(grid_len):
+            u_t = linear_actions(com[:,i,j]) # compute optimal control
+            state_diff = linear_dynamics(state=com[:,i,j], action=u_t)
+            #adjust by delta t, discretization
+            Fx[i, j] = fpk_dt * state_diff[0]
+            Fy[i, j] = fpk_dt * state_diff[1]
+            pass
+    return np.array([Fx, Fy])
 
+""" use CARE to solve for suboptimal control for linear systems"""
 def linear_actions(state):
     R = 3
     q = np.array([[1, 0], [0, 1]])
@@ -20,7 +49,6 @@ def linear_actions(state):
     return np.dot(-k, state)
 
 
-dt = 0.1
 # generate a new agent
 i_agent = Agent(init_state=np.array([[1], [1]]),
                 init_action=np.array([[2], [2]]),
@@ -33,8 +61,7 @@ states = []
 for i in range(steps):
     action = linear_actions(i_agent.state)
     states.append(i_agent.move_steps(action=np.asarray(action), steps=1)[0])
-print(i_agent.state)
-this_state = np.stack(states)
-plt.plot(this_state[:, 0, 0])
-plt.plot(this_state[:, 1, 0])
-plt.show()
+
+#simply plot the states
+utilities.simple_plot_states(states)
+pass
--- src/agent/src/utilities.py
+++ src/agent/src/utilities.py
@@ -1,4 +1,18 @@
 # 2020-12-14 the utility functions for Agent package. Created and maintained by Zejian Zhou
 
+import matplotlib.pyplot as plt
+import numpy as np
 
-pass
+""" simply plot the states
+    states -- list of ndarray, list length is the step number
+"""
+def simple_plot_states(states):
+    this_state = np.stack(states).squeeze(2) #squeeze to eliminate the extra dimension
+    legend_list = []
+    for i in range(this_state.shape[1]):
+        temp, =plt.plot(this_state[:, i], label = "state"+str(i))
+        legend_list.append(temp)
+
+    plt.legend(handles=legend_list, loc='upper right')
+    plt.title("All states plot")
+    plt.show()
