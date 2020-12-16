1a6f8b1834ed535aa488eb71c8cd007f5b9955fc
zhouzejian1994@gmail.com <rnqPAuYKC?2y6wGeARU=83DQ>
2020-12-16 10:43:28 -0800

    adjust code format

--- src/agent/src/main_tester.py
+++ src/agent/src/main_tester.py
@@ -4,35 +4,37 @@ import numpy as np
 from scipy import linalg
 import matplotlib.pyplot as plt
 
-def linear_dynamics (state, action):
-    a,b=np.array([[-3,2],[1,1]]), [[0],[1]]
-    return (np.dot(a,state) + b*action)
 
-def linear_actions (state):
+def linear_dynamics(state, action):
+    a, b = np.array([[-3, 2], [1, 1]]), [[0], [1]]
+    return (np.dot(a, state) + b * action)
+
+
+def linear_actions(state):
     R = 3
-    q = np.array([[1,  0], [0, 1]])
+    q = np.array([[1, 0], [0, 1]])
     a = np.array([[-3, 2], [1, 1]])
-    b= np.array([[0], [1]])
-    P = linalg.solve_continuous_are (a, b, q, R)
-    k= 1/R * np.dot(np.transpose(b), P)
-    return np.dot(-k,state)
+    b = np.array([[0], [1]])
+    P = linalg.solve_continuous_are(a, b, q, R)
+    k = 1 / R * np.dot(np.transpose(b), P)
+    return np.dot(-k, state)
 
 
-dt=0.1
+dt = 0.1
 # generate a new agent
-i_agent = Agent(init_state = np.array([[1],[1]]),
-                                      init_action = np.array([[2],[2]]),
-                                      dt=dt,
-                                      dynam = linear_dynamics)
+i_agent = Agent(init_state=np.array([[1], [1]]),
+                init_action=np.array([[2], [2]]),
+                dt=dt,
+                dynam=linear_dynamics)
 
-#generate test action list
+# generate test action list
 steps = 200
-states=[]
+states = []
 for i in range(steps):
     action = linear_actions(i_agent.state)
-    states.append(i_agent.move_steps(action= np.asarray(action), steps=1)[0])
+    states.append(i_agent.move_steps(action=np.asarray(action), steps=1)[0])
 print(i_agent.state)
-this_state=np.stack(states)
-plt.plot(this_state[:,0,0])
-plt.plot(this_state[:,1,0])
+this_state = np.stack(states)
+plt.plot(this_state[:, 0, 0])
+plt.plot(this_state[:, 1, 0])
 plt.show()
